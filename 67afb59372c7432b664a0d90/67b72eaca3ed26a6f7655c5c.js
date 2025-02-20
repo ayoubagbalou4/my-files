1d35b5b49badaf343f4e70977e7966ba3c9c1981
Ayoub Agbalou <149244056+ayoubagbalou4@users.noreply.github.com>
2025-02-20 14:33:26 +0100

    Delete file 67b72eaca3ed26a6f7655c5c.js in 67afb59372c7432b664a0d90

--- 67afb59372c7432b664a0d90/67b72eaca3ed26a6f7655c5c.js
+++
@@ -1,7 +0,0 @@
-
-            const mongoose = require('mongoose');
-            const userModel = mongoose.model('user67b72eaca3ed26a6f7655c5c', {
-                
-            });
-            module.exports = userModel;
-        
