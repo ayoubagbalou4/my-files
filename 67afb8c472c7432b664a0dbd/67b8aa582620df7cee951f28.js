61ca8b1691da013b0d73e99a90d9a39f8972b67c
Ayoub Agbalou <149244056+ayoubagbalou4@users.noreply.github.com>
2025-02-21 17:31:20 +0100

    Upload file 67b8aa582620df7cee951f28.js in 67afb8c472c7432b664a0dbd

---
+++ 67afb8c472c7432b664a0dbd/67b8aa582620df7cee951f28.js
@@ -0,0 +1,11 @@
+
+            const mongoose = require('mongoose');
+            const mModel = mongoose.model('m67b8aa582620df7cee951f28', {
+                
+                            kk: {
+                                type: String,
+                                required: [false, 'kk is required'],
+                            }
+            });
+            module.exports = mModel;
+        
