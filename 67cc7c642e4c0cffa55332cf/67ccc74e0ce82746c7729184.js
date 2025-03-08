415061e2e469c5e7bda34d69018657fefae5ba82
Ayoub Agbalou <149244056+ayoubagbalou4@users.noreply.github.com>
2025-03-08 22:40:14 +0000

    Upload file 67ccc74e0ce82746c7729184.js in 67cc7c642e4c0cffa55332cf

---
+++ 67cc7c642e4c0cffa55332cf/67ccc74e0ce82746c7729184.js
@@ -0,0 +1,11 @@
+
+            const mongoose = require('mongoose');
+            const nnnModel = mongoose.model('nnn67ccc74e0ce82746c7729184', {
+                
+                            ss: {
+                                type: String,
+                                required: [false, 'ss is required'],
+                            }
+            });
+            module.exports = nnnModel;
+        
