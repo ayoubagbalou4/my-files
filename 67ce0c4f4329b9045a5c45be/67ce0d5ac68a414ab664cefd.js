97183f2e06c9b43f90b570b9a32a9c50fce71596
Ayoub Agbalou <149244056+ayoubagbalou4@users.noreply.github.com>
2025-03-09 21:51:23 +0000

    Upload file 67ce0d5ac68a414ab664cefd.js in 67ce0c4f4329b9045a5c45be

---
+++ 67ce0c4f4329b9045a5c45be/67ce0d5ac68a414ab664cefd.js
@@ -0,0 +1,11 @@
+
+            const mongoose = require('mongoose');
+            const txvModel = mongoose.model('txv67ce0d5ac68a414ab664cefd', {
+                
+                            kk: {
+                                type: String,
+                                required: [false, 'kk is required'],
+                            }
+            });
+            module.exports = txvModel;
+        
