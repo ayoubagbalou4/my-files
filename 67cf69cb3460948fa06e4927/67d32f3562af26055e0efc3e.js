212ee524cbfa445754c36f4d8ee3e26f6739265d
Ayoub Agbalou <149244056+ayoubagbalou4@users.noreply.github.com>
2025-03-13 19:17:10 +0000

    Upload file 67d32f3562af26055e0efc3e.js in 67cf69cb3460948fa06e4927

---
+++ 67cf69cb3460948fa06e4927/67d32f3562af26055e0efc3e.js
@@ -0,0 +1,12 @@
+
+            const mongoose = require('mongoose');
+            const tessssModel = mongoose.model('tessss67d32f3562af26055e0efc3e', {
+                
+                            hii: {
+                                type: String, // Assuming storing image as a URL or path
+                                required: [false, 'hii is required'],
+                                emum:[hoo,xx]
+                            }
+            });
+            module.exports = tessssModel;
+        
