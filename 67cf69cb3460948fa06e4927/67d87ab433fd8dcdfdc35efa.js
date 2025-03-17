39110ad2e0d055b2f7707460b6bae079c822a43e
Ayoub Agbalou <149244056+ayoubagbalou4@users.noreply.github.com>
2025-03-17 20:57:44 +0000

    Upload file 67d87ab433fd8dcdfdc35efa.js in 67cf69cb3460948fa06e4927

---
+++ 67cf69cb3460948fa06e4927/67d87ab433fd8dcdfdc35efa.js
@@ -0,0 +1,24 @@
+
+            const mongoose = require('mongoose');
+            const usersModel = mongoose.model('users67d87ab433fd8dcdfdc35efa', {
+                
+                            sx: {
+                                type: String, 
+                                required: [false, 'sx is required'],
+                                enum: ["f", "fd"]
+                            },
+                            cd: {
+                                type: String,
+                                required: [false, 'cd is required'],
+                            },
+                            test: {
+                                type: String,
+                                required: [false, 'test is required'],
+                            },
+                            ayoub: {
+                                type: String,
+                                required: [false, 'ayoub is required'],
+                            }
+            });
+            module.exports = usersModel;
+        
