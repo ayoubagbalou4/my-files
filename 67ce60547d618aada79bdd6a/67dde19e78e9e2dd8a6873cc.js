98b4919f9985c0278eb17c0773e746abbf5970a0
Ayoub Agbalou <149244056+ayoubagbalou4@users.noreply.github.com>
2025-03-21 22:01:02 +0000

    Upload file 67dde19e78e9e2dd8a6873cc.js in 67ce60547d618aada79bdd6a

---
+++ 67ce60547d618aada79bdd6a/67dde19e78e9e2dd8a6873cc.js
@@ -0,0 +1,7 @@
+
+            const mongoose = require('mongoose');
+            const okkModel = mongoose.model('okk67dde19e78e9e2dd8a6873cc', {
+                
+            });
+            module.exports = okkModel;
+        
