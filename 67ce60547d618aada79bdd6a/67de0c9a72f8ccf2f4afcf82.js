9076263190aa158c8a6a43bff1b4cd28e7193162
Ayoub Agbalou <149244056+ayoubagbalou4@users.noreply.github.com>
2025-03-22 01:04:26 +0000

    Upload file 67de0c9a72f8ccf2f4afcf82.js in 67ce60547d618aada79bdd6a

---
+++ 67ce60547d618aada79bdd6a/67de0c9a72f8ccf2f4afcf82.js
@@ -0,0 +1,7 @@
+
+            const mongoose = require('mongoose');
+            const ilyasModel = mongoose.model('ilyas67de0c9a72f8ccf2f4afcf82', {
+                ,
+            });
+            module.exports = ilyasModel;
+        
