9022d87d4033e397819d651293fb9e45c61c9d3a
Ayoub Agbalou <149244056+ayoubagbalou4@users.noreply.github.com>
2025-04-12 22:24:05 +0100

    Upload file 67fad9f4e02f423a4713f57b.js in 67fad808e02f423a4713f47b

---
+++ 67fad808e02f423a4713f47b/67fad9f4e02f423a4713f57b.js
@@ -0,0 +1,11 @@
+
+            const mongoose = require('mongoose');
+            const ttModel = mongoose.model('tt67fad9f4e02f423a4713f57b', {
+                
+                            s: {
+                                type: String,
+                                required: [false, 's is required'],
+                            }
+            });
+            module.exports = ttModel;
+        
