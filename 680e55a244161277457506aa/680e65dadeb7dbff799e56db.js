7ed804705d0c2d8bc8522c64d44b0e8c2458e745
Ayoub Agbalou <149244056+ayoubagbalou4@users.noreply.github.com>
2025-04-27 18:24:20 +0100

    Upload file 680e65dadeb7dbff799e56db.js in 680e55a244161277457506aa

---
+++ 680e55a244161277457506aa/680e65dadeb7dbff799e56db.js
@@ -0,0 +1,34 @@
+
+            const mongoose = require('mongoose');
+            const userModel = mongoose.model('user680e65dadeb7dbff799e56db', {
+                
+                            name: {
+                                type: String,
+                                required: [false, 'name is required'],
+                                minlength: [null, 'name must be at least null characters'],
+                                maxlength: [null, 'name cannot exceed null characters']
+                            },
+                            age: {
+                                type: Number,
+                                required: [true, 'age is required'],
+                                min: [18, 'age must be at least 18'],
+                                max: [65, 'age cannot exceed 65']
+                            },
+                            email: {
+                                type: String,
+                                required: [false, 'email is required'],
+                                minlength: [null, 'email must be at least null characters'],
+                                maxlength: [null, 'email cannot exceed null characters']
+                            },
+                            gender: {
+                                type: String, 
+                                required: [false, 'gender is required'],
+                                enum: ["male", "female"]
+                            },
+                            birthday: {
+                                type: Date,
+                                required: [false, 'birthday is required']
+                            }
+            });
+            module.exports = userModel;
+        
