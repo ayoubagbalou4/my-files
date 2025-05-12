4141b7014363916dccb5b8cb62c513424ede9e66
Ayoub Agbalou <149244056+ayoubagbalou4@users.noreply.github.com>
2025-05-12 13:00:51 +0100

    Upload file 6821e2f3c9732af919a707ad.js in 6821d5020bfc3b660b28a7b8

---
+++ 6821d5020bfc3b660b28a7b8/6821e2f3c9732af919a707ad.js
@@ -0,0 +1,25 @@
+
+            const mongoose = require('mongoose');
+            const userModel = mongoose.model('user6821e2f3c9732af919a707ad', {
+                
+                            name: {
+                                type: String,
+                                required: [false, 'name is required'],
+                                minlength: [null, 'name must be at least null characters'],
+                                maxlength: [null, 'name cannot exceed null characters']
+                            },
+                            address: {
+                                type: String,
+                                required: [false, 'address is required'],
+                                minlength: [null, 'address must be at least null characters'],
+                                maxlength: [null, 'address cannot exceed null characters']
+                            },
+                            phone: {
+                                type: Number,
+                                required: [false, 'phone is required'],
+                                min: [null, 'phone must be at least null'],
+                                max: [null, 'phone cannot exceed null']
+                            }
+            });
+            module.exports = userModel;
+        
